7e525b126f26352c86b552d9fec52241ae808221
ivan-shumiln <shumilin.i.v@gmail.com>
2022-05-16 14:22:35 +0300

    сверстал тестовый интерфейс

---
+++ front/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class NameForm(forms.Form):
+    pass
+    # hidden = forms.CharField(max_length=100)
--- front/views.py
+++ front/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import NameForm
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return render(request, 'front/index.html', {'form': form})
+    else:
+        form = NameForm()
+    return render(request, 'front/index.html', {'form': form})
